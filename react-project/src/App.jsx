054f097e2a10d8209f413f44e40246184bbdf0e7
MoloroMalepe <moloromalepe17@gmail.com>
2024-10-31 14:31:00 +0200

    useEffect for data fetching

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,33 +1,28 @@
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { useState } from "react";
-import React from "react";
 import "./App.css";
 
-function CongratulationsMessage({ name }) {
-  return <p>Congratulations {name}!</p>;
-}
-
-function ChangeNameButton({ onChangeName }) {
-  return <button onClick={onChangeName}>Change winner</button>;
-}
-
 function App() {
-  const [name, setName] = useState("Steph");
+  // Initialized as an empty array
+  const [data, setData] = useState([]);
 
-  React.useEffect(() => {
-    document.title = `Celebrate, ${name}!`;
-  }, [name]);
-
-  const handleChangeName = () => {
-    setName("Lori");
-  };
-
-  return (
-    <div>
-      <CongratulationsMessage name={name} />
-      <ChangeNameButton onChangeName={handleChangeName} />
-    </div>
-  );
+  //using useEffect to fetch the data from the api
+  useEffect(() => {
+    fetch(`https://api.github.com/users`)
+      .then((response) => response.json())
+      .then((res) => setData(res));
+    // .then( setData);
+  });
+  if (data) {
+    return (
+      <ul>
+        {data.map((user) => (
+          <li>{user.message}</li>
+        ))}
+      </ul>
+    );
+  }
+  return <p>No users</p>;
 }
 
 export default App;
